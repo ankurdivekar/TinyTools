cd8c2fe2c37f9ec8224e89047101a36bcd6af379
Ankur Divekar <50209588+ankurdivekar@users.noreply.github.com>
2024-07-23 12:26:23 +0530

    Update MouseJiggler.py
    Added mouse listener

--- MouseJiggler.py
+++ MouseJiggler.py
@@ -4,9 +4,17 @@ import sys
 import msvcrt
 from datetime import datetime
 from pynput.keyboard import Key, Listener
+from random import randint
+from pynput import mouse
 
 
-def on_press(key):
+def on_mouse_activity(x, y):
+    global sec_counter
+    sec_counter = 0
+    # print("sec counter reset")
+
+
+def on_keyboard_press(key):
 
     global sec_counter
     global init_pos
@@ -23,34 +31,58 @@ def on_press(key):
 #     print("{0} release".format(key))
 
 
-def wiggle_mouse(init_pos):
+def wiggle_mouse(init_pos, screen_width, screen_height):
+    global sec_counter
+    move_duration = 0.1
 
-    pyautogui.moveTo(1, 995)
-    pyautogui.click()
+    for i in range(5):
+        if sec_counter >= check_delay_sec:
+            random_x = randint(0, screen_width)
+            random_y = randint(0, screen_height)
+            pyautogui.moveTo(random_x, random_y, duration=move_duration, tween=pyautogui.easeInOutQuad)
+        else:
+            return
+    pyautogui.moveTo(1350, 1050, duration=move_duration, tween=pyautogui.easeInOutQuad)
     # time.sleep(1)
-    pyautogui.moveTo(1, 997)
-    # time.sleep(1)
     pyautogui.click()
-    # pyautogui.moveTo(1, 1079)
-    pyautogui.moveTo(init_pos)
+
+    # pyautogui.moveTo(1340, 1050, duration=move_duration)
+    # pyautogui.click()
+    # # time.sleep(1)
+
+    # # pyautogui.moveTo(1, 1079),
+    # pyautogui.moveTo(init_pos)
     # time.sleep(1)
-    pyautogui.click()
-    print(f"Movement made at {datetime.now().time():%I:%M:%S %p}")
+    # pyautogui.click()
+    print(f"Movement {movement_counter} made at {datetime.now().time():%I:%M:%S %p}")
 
 
 pyautogui.FAILSAFE = False
-check_delay_sec = 118
+check_delay_sec = 110
 
 global sec_counter
+global movement_counter
 # global init_pos
 
 sec_counter = 0
-listener = Listener(
-    on_press=on_press,
+movement_counter = 0
+keyboard_listener = Listener(
+    on_press=on_keyboard_press,
     # on_release=on_release,
 )
-listener.start()
-print("Monitoring activity...")
+keyboard_listener.start()
+
+mouse_listener = mouse.Listener(
+    on_move=on_mouse_activity,
+    on_click=on_mouse_activity,
+    on_scroll=on_mouse_activity,
+)
+mouse_listener.start()
+
+# print("Monitoring activity...")
+screenWidth, screenHeight = pyautogui.size()
+
+print(f"Screen width: {screenWidth}, Screen height: {screenHeight}")
 
 while True:
     init_pos = pyautogui.position()
@@ -61,8 +93,9 @@ while True:
 
     if init_pos == new_pos:
         if sec_counter >= check_delay_sec:
-            # print("Moving mouse")
-            wiggle_mouse(init_pos)
+            movement_counter += 1
+            # print(f"Movement {movement_counter}")
+            wiggle_mouse(init_pos, screen_width=screenWidth, screen_height=screenHeight)
             sec_counter = 0
     else:
         sec_counter = 0
